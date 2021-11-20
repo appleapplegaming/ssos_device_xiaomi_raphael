5685596f554e53db819e271bdc46a6b72a965e38
appleapplegaming <abou08923@gmail.com>
2021-11-20 18:38:28 +0000

    uses awaken gapps

--- device.mk
+++ device.mk
@@ -455,3 +455,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+$(call inherit-product-if-exists, vendor/gms/products/gms.mk)
